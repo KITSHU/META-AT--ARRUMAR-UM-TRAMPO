774cd8656821a22cb76279b54efc3a807a3ff5c8
lucas <lucas2015kaka@gmail.com>
2024-08-14 14:34:42 -0300

    começar

---
+++ SEÇÃO 2/aula 1/index.js
@@ -0,0 +1,9 @@
+const nome = 'lucas'
+const idade = 20
+const email = 'lucas2015kaka@gmail.com'
+const altura = 1.80
+
+console.log(nome)
+console.log(email)
+console.log(idade)
+console.log(altura)

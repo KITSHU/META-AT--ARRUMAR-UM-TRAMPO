e89b614b1222c443962a2c575edcf77fc37966b4
lucas <lucas2015kaka@gmail.com>
2024-08-15 18:20:26 -0300

    const e let

---
+++ SEÇÃO 2/aula 4/index.js
@@ -0,0 +1,10 @@
+let nome
+nome = 'João'
+console.log(nome);
+nome = 'Lucas'
+console.log(nome);
+let nome0
+nome0 = 'vitor'
+console.log(nome0);
+let nomeCliente = 'Carla'
+console.log(nomeCliente);
---
+++ SEÇÃO 2/aula 5/index.js
@@ -0,0 +1,6 @@
+const nome = 10
+const nome1 = 2000
+const nome2 = nome1 * nome
+const nome3 = nome2 / 2
+let nome4 = nome2 *3
+console.log(nome4);
